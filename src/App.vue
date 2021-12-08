acd825c0f4028b4e694ce71adf4781e4c75076e2
ayushghetia <55269750+ghetiaayush@users.noreply.github.com>
2021-12-08 12:20:22 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <Slider/>
+</template>
+
+<script>
+import Slider from './components/Slider.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Slider
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0px;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
